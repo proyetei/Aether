ffcfb63d1fea5f397cf9be1b6989810d980491d7
Proyetei <proyeteireavue@gmail.com>
2024-08-13 17:04:55 -0400

    fix bugs

--- next.config.js
+++ next.config.js
@@ -8,14 +8,6 @@ const nextConfig = {
             }
         ]
     },
-    async rewrites() {
-        return [
-            {
-                source: '/api/:path*',
-                destination: 'http://localhost:8080/api/:path*',
-            },
-        ]
-    }
 }
 
 module.exports = nextConfig
