3407e278c7ffc76caf370c173e9c381cd0f24144
Proyetei <proyeteireavue@gmail.com>
2024-08-20 03:46:58 -0400

    develop entry form for adding journal and develop functionality for viewing entries using PostgreSQL with supabase

---
+++ components/explore/Display.tsx
@@ -0,0 +1,116 @@
+
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import EditButton from '../buttons/EditButton';
+import DeleteButton from '../buttons/DeleteButton';
+import { subTitle } from '@/fonts/font';
+import { dateFormat } from '@/lib/dateFormat';
+import Masonry from '@mui/lab/Masonry';
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination"
+import {
+  Card,
+  CardContent,
+  CardFooter,
+} from "@/components/ui/card"
+import ViewModal from '../ViewModal';
+import { Separator } from '../ui/separator';
+import { useSearchParams } from "next/navigation";
+import { Database } from '@/database.types';
+interface DisplayProps{
+  entries: Database['public']['Tables']['entries']['Row'][]
+}
+const Display: React.FC<DisplayProps> = ({entries}) => {
+
+  // const [journal, setJournal] = useState<Entry[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(8);
+  const searchParams = useSearchParams();
+  const query = searchParams.get('query')?.toLowerCase() || '';
+  const filteredEntries = entries.filter(entry => 
+    entry.entry.toLowerCase().includes(query) || 
+    entry.question?.toLowerCase().includes(query)
+  );
+
+  const totalItems = filteredEntries.length;
+  const firstItemIndex = totalItems - currentPage * itemsPerPage; 
+  const lastItemIndex = firstItemIndex + itemsPerPage;
+  const currentItems = filteredEntries.slice(Math.max(firstItemIndex, 0), lastItemIndex).reverse();
+
+  return (
+    <>
+    <Masonry columns={{xs:2, md:4}} spacing={1} >
+      {currentItems?.map((entry, index) => { // map each entry item to its index
+        const creationDate = entry?.created_at
+        const entryTitle = entry?.title
+        return (
+
+          <Card key={index}>
+            <div className='bg-white/20 backdrop-blur-lg rounded-xl px-1 md:text-lg text-sm'>
+              <p className={`${subTitle.className} p-1`}> { entry && entryTitle} </p>
+              <CardContent className=' min-h-[100px] bg-zinc-900 text-slate-200 p-3'>
+                  <p className='pb-2'> {entry ? entry.entry : ''} </p>
+                  <p> {!entry.question ? '' : ( <> <Separator className='bg-zinc-600 my-2' /> <p> Question chosen: </p>  - {entry.question} </> )} </p>
+              </CardContent>
+              <CardFooter>
+              <p className={`${subTitle.className} text-sm opacity-70 p-1`}> 
+                {entry && creationDate ? `${dateFormat(new Date(creationDate).toISOString())}`: ''}
+              </p>
+              <div className='flex flex-row text-[#a8b0d3]'>
+                  {/* {entry && <DeleteButton post = {entry}/>} */}
+                  {/* {entry && <EditButton post={entry} />} */}
+                  {entry && <ViewModal post={entry} />}
+              </div> 
+              </CardFooter> 
+              </div>
+          </Card>
+        );
+      })}
+    </Masonry>
+    <PaginationSection totalItems={totalItems} itemsPerPage={itemsPerPage} currentPage={currentPage} setCurrentPage={setCurrentPage}/>
+    </>
+  );
+}
+
+function PaginationSection({
+  totalItems, itemsPerPage, currentPage, setCurrentPage
+}:{
+  totalItems: any;
+  itemsPerPage: any;
+  currentPage: any;
+  setCurrentPage: any;
+}){
+  let pages = [];
+  for (let i =1; i <= Math.ceil(totalItems / itemsPerPage); i++){
+    pages.push(i);
+  }
+  const handleNextPage = () => {
+    if (currentPage < pages.length ){
+      setCurrentPage(currentPage + 1)
+    }
+  }
+  const handlePrevPage = () => {
+    if(currentPage > 1){
+      setCurrentPage(currentPage - 1);
+    }
+  }
+  return(
+    <Pagination>
+  <PaginationContent className='gap-4 py-4'>
+    <PaginationItem className='bg-gradient-to-r from-blue-400 to-pink-400 hover:drop-shadow-blue rounded-lg text-slate-50'>
+      <PaginationPrevious onClick={() => handlePrevPage()} />
+    </PaginationItem>
+    <PaginationItem className='bg-gradient-to-r from-blue-400 to-pink-400 hover:drop-shadow-blue rounded-lg text-slate-50'>
+      <PaginationNext onClick={() => handleNextPage()} />
+    </PaginationItem>
+  </PaginationContent>
+</Pagination>
+
+  )
+}
+export default Display
--- components/forms/EntryForm.tsx
+++ components/forms/EntryForm.tsx
@@ -1,10 +1,8 @@
 "use client"
-import React, { useEffect, useState } from "react"
+import React, { useState } from "react"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import * as z from "zod"
-import axios from 'axios';
-import { Button } from "@/components/ui/button"
 import {
   Form,
   FormControl,
@@ -13,49 +11,79 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form"
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select"
 import { Textarea } from "@/components/ui/textarea"
 import { useToast } from "@/components/ui/use-toast"
-import {  bodyText, subTitle } from "@/fonts/font"
+import { subTitle } from "@/fonts/font"
 import { redirect, useRouter } from "next/navigation"
 import { FormSchema } from "@/lib/formValidation"
-import { questionBank } from "@/lib/data"
-import { Check, ChevronsUpDown } from "lucide-react"
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover"
-import { cn } from "@/lib/utils"
-import { ScrollArea } from "@/components/ui/scroll-area"
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
 import SubmitButton from "../buttons/SubmitButton"
-import QuestionGenerator from "../QuestionGenerator"
-import { useQuestion } from "../QuestionContext"
+import { Input } from "../ui/input"
+import { useUser, useSession } from "@clerk/nextjs"
+import { createClient } from '@supabase/supabase-js'
+import { Database } from "@/database.types";
+
 
 const EntryForm: React.FC = () => {
   
   const router = useRouter();
   const { toast } = useToast();
-  const { selectedQuestion } = useQuestion();
+  const { user } = useUser();
+
   const [selectedValue, setSelectedValue] = useState(false);
 
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
   })
- 
+
+  // The `useSession()` hook will be used to get the Clerk session object
+  const { session } = useSession()
+
+// Create a custom supabase client that injects the Clerk Supabase token into the request headers
+function createClerkSupabaseClient() {
+  return createClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_KEY!,
+    {
+      global: {
+        // Get the custom Supabase token from Clerk
+        fetch: async (url, options = {}) => {
+          const clerkToken = await session?.getToken({
+            template: 'supabase',
+          })
+
+          // Insert the Clerk Supabase token into the headers
+          const headers = new Headers(options?.headers)
+          headers.set('Authorization', `Bearer ${clerkToken}`)
+
+          // Now call the default fetch
+          return fetch(url, {
+            ...options,
+            headers,
+          })
+        },
+      },
+    },
+  )
+}
+
+  const client = createClerkSupabaseClient()
   const onSubmit = async (data: z.infer<typeof FormSchema>) => {
     try {
-      const promptAnswered = !!data.question; // Simplified boolean check
-      await axios.post("/api", {entry: data.entry, selection: data.selection, question: selectedQuestion });
-      await axios.post("/api/points", {promptAnswered} )
-      await axios.get("/api", {});
+
+      if (!user) {
+        return redirect("/");
+      }
+
+      await client.from('entries').insert([
+        {title: data.title, entry: data.entry, selection: data.selection, question: data?.question, user_id: user.id }
+      ])
+
+      console.log("Here are your form values:", data.title, data.entry, data.selection, data?.question, user.id )
+
+      // await client.from('users').update([{userPoints: user.userPoints + 3,}]);
+
+
       form.reset();
       router.push("/explore")
       toast({
@@ -72,8 +100,31 @@ const EntryForm: React.FC = () => {
   }
   return (
     <div className=" items-center justify-center md:p-12 px-4">
+
+      <p className={` ${subTitle.className} md:text-xl text-base text-violet-500`}>You will be awarded +5 points! </p>
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className=" space-y-6">
+      <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel> 
+                <div className={` ${subTitle.className} pb-2 md:p-4 rounded-lg text-lg`}> 
+                Please enter a title* (25 characters max.)
+                </div> 
+              </FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="Title..."
+                  className="md:w-2/5 w-full rounded-sm border-indigo-600 border-2 bg-white/10 backdrop-blur-xl backdrop-saturate-200 text-white/90 px-3 py-1.5"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
         <FormField
           control={form.control}
           name="entry"
@@ -81,7 +132,7 @@ const EntryForm: React.FC = () => {
             <FormItem>
               <FormLabel> 
                 <div className={` ${subTitle.className} pb-2 md:p-4 rounded-lg text-lg`}> 
-                Journal entry
+                Journal entry* (250 characters max.)
                 </div> 
               </FormLabel>
               <FormControl>
@@ -130,7 +181,27 @@ const EntryForm: React.FC = () => {
             </FormItem>
           )}
         />
-          { selectedValue ? <QuestionGenerator /> : null }
+        <FormField
+          control={form.control}
+          name="question"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel> 
+                <div className={` ${subTitle.className} pb-2 md:p-4 rounded-lg text-lg`}>
+                What are three things you <span className="text-violet-500"> love </span> about yourself? (Optional, 100 characters max.)
+                </div> 
+              </FormLabel>
+              <FormControl>
+                <Textarea
+                  placeholder="Answer here"
+                  className="md:w-4/5 w-full h-[200px] rounded-sm border-indigo-600 border-2 bg-white/10 backdrop-blur-xl backdrop-saturate-200 text-white/90 px-3 py-1.5"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
         </div>
         <div className="items-center justify-center text-center pt-6">
           <SubmitButton placeholder="Submit entry" />
